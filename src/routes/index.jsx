59c9d9e8ea719e7a0394cf4da9ff47ed5c0b5b8e
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-08-26 15:51:33 -0300

    Update index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -8,13 +8,12 @@ import { useEffect } from 'react';
 import { api } from '../services/api';
 
 export function Routes() {
-    const { user, admin, signOut } = useAuth();
+    const { user, admin } = useAuth();
 
     useEffect(() => {
         api.get('/users/validated').catch((error) => {
             if (error.response?.status == 401) {
                 console.log(error);
-                signOut()
             }
         })
     }, [])
