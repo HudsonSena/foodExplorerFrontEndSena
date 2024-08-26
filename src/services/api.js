2c3c9f6a349dde434ebf968e083989cc83c276d1
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-08-26 15:48:49 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://foodexplorerbackendapisena.onrender.com",
 });
 //http://localhost:3333
 //https://foodexplorerbackendapisena.onrender.com
